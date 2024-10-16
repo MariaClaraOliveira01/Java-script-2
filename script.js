8edf9fa201894985e9efabcd790b98bb415bb180
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-10-16 08:52:03 -0300

    oi

---
+++ script.js
@@ -0,0 +1,11 @@
+function alteraCirculo(){
+    const circulo= document.getElementByld('circulo')
+
+    const corAleatoria=  '#' + 
+    Math.floor(Math.random()*16777215).toString(16);
+
+    const novoTamanho= Math.floor(Math.random()* (200 - 50 + 1)) + 50;
+    circulo.style.backgroundColor= corAleatoria;
+    circulo.style.width= novoTamanho + 'px';
+    circulo.style.heigth= novoTamanho +  'px';
+}
