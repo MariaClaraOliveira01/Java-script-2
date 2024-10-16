65ec84da31c494f7605cc452cbe224d4640e9844
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-10-16 09:11:17 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function alterarCirculo(){
     const circulo= document.getElementByld('circulo')
 
-    const corAleatoria=  '#' + 
+    const corAleatoria =  '#' + 
     Math.floor(Math.random()*16777215).toString(16);
 
-    const novoTamanho= Math.floor(Math.random()* (200 - 50 + 1)) + 50;
-    circulo.style.backgroundColor= corAleatoria;
-    circulo.style.width= novoTamanho + 'px';
-    circulo.style.heigth= novoTamanho +  'px';
+    const novoTamanho = Math.floor(Math.random()* (200 - 50 + 1)) + 50;
+    circulo.style.backgroundColor = corAleatoria;
+    circulo.style.width = novoTamanho + 'px';
+    circulo.style.heigth = novoTamanho +  'px';
 }
